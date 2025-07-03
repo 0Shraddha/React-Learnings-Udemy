33354bca02b6f51436648ee9c02952e6e1e8a5c0
Shraddha Dongol <shraddhadongol@Shraddhas-MacBook-Air.local>
2025-07-03 23:28:54 +0545

    started a project mgmt tool project

---
+++ 05-project-mgmt/src/App.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
+import ProjectSidebar from "./components/ProjectSidebar";
+
+function App() {
+ const [projectsState, setProjectsState] = useState({
+  selectedProjectId : undefined,
+  projects: []
+ });
+
+ function handleStartAddProject(){
+  setProjectsState(prevState => {
+    return {
+      ...prevState,
+      selectedProjectId: null,
+    }
+  })
+ }
+
+ function handleAddProject(projectData){
+  console.log("New project added : ", projectData)
+  setProjectsState(prevState => {
+    const newProject = {
+      ...projectData,
+      id: Math.random()
+    }
+
+    return{
+      ...prevState,
+      projects: [...prevState.projects, newProject]
+    }
+  })
+ }
+
+ console.log(projectsState)
+
+ let content;
+ if(projectsState.selectedProjectId === null){
+  content = <NewProject onAdd={handleAddProject} />
+ }else if(projectsState.selectedProjectId === undefined){
+  content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>
+ }
+
+
+  return (
+    <>
+      <main className="h-screen mb-8 flex gap-10">
+        <ProjectSidebar onStartAddProject={handleStartAddProject}/>
+       {content}
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ 05-project-mgmt/src/components/ProjectSidebar.jsx
@@ -0,0 +1,11 @@
+import Button from "./Button";
+
+export default function ProjectSidebar({onStartAddProject}){
+    return (
+        <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl ">
+            <h1 className="mb-8 font-bold uppercase md:text-xl text-stone-200">My porjects</h1>
+           <Button onClick={onStartAddProject}> + Add Project</Button>
+
+        </aside>
+    )
+}
